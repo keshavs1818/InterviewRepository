9f710e9fdafec8a07d6e557b65e13f41290670b5
Keshav Subramaniyam <krs227@scarletmail.rutgers.edu>
2023-10-05 17:26:58 -0400

    Version 3

--- App/minimumcost.py
+++ App/minimumcost.py
@@ -12,10 +12,10 @@ def minimumcost(array):
     pairs_hash = {}
     for i in range(0, len(array) - 1):
         new_arr = array.copy()
-        median = math.floor((new_arr[i] + new_arr[i + 1]) / 2)
+        median = math.floor((new_arr[i] + new_arr[i + 1]) / 3)
         new_arr.insert(i + 1, median)
         new_lst = [(new_arr[i] - new_arr[i + 1]) ** 2 for i in range(len(new_arr) - 1)]
         pairs_hash[(i, i + 1)] = sum(new_lst)
-        cost = min(cost, pairs_hash[(i, i + 1)])
+        cost = min(cost, pairs_hash[(i, i + 1)])   
     return cost
 print(minimumcost([0, 1, 5, 6, 8]))
